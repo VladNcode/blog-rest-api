5b11925ce1bd60aaa55e15cd7773aa66a24ed378
VladNCode <vldsmrfow@gmail.com>
2021-12-27 08:10:22 +0200

    added file upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: './config.env' });
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -22,6 +23,22 @@ app.use('/api/v1/auth/', authRouter);
 app.use('/api/v1/users/', userRouter);
 app.use('/api/v1/posts/', postRouter);
 app.use('/api/v1/categories/', categoryRouter);
+
+//* Upload files
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'images'),
+  filename: (req, file, cb) => cb(null, 'hello.jpg'),
+});
+
+const upload = multer({ storage });
+
+app.post('/api/v1/upload/', upload.single('file'), (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'File uploaded successfully',
+  });
+});
 
 //* Error handling
 app.all('*', (req, res, next) => {
